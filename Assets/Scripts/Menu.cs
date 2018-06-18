9e414cd93b9c14e81fa49d49cf8cd7d7b06088f7
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-06-18 12:51:50 +0200

    StartScreen fertig

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    
+    public void StartCatchGame()
+    {
+        SceneManager.LoadScene("CatchGame");
+    }
+
+    public void StartDodgeBall()
+    {
+        SceneManager.LoadScene("DodgeBall");
+    }
+}
