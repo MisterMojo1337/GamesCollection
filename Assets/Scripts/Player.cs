6f5ca6790cadfff06e482bfaf83a67fdc3b672ed
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-06-21 07:39:54 +0200

    V1.0 rdy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,29 +6,37 @@ using System.Linq;
 public class Player : MonoBehaviour {
     
     public GameObject graveStone;
-
     public Rigidbody2D rb;
+
     public float movementForce = 30f;
     public float jumpForce = 20f;
+    private float distToGround;    
+    
 
     private void Start()
     {
+        distToGround = transform.position.y;
     }
     // Update is called once per frame
     void Update () {
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(new Vector2(movementForce * Time.deltaTime, 0), ForceMode2D.Force);
+            //rb.AddForce(new Vector2(movementForce * Time.deltaTime, 0), ForceMode2D.Force);
+            transform.Translate(new Vector3(movementForce * Time.deltaTime, 0));
         }
         if (Input.GetKey("a")) 
         {
-            rb.AddForce(new Vector2(-movementForce * Time.deltaTime, 0), ForceMode2D.Force);
+            //rb.AddForce(new Vector2(-movementForce * Time.deltaTime, 0), ForceMode2D.Force);
+            transform.Translate(new Vector3(-movementForce * Time.deltaTime, 0));
         }
         if (Input.GetKey("w"))
         {
-            //rb.velocity += (jumpForce * Vector2.up) * Time.deltaTime;
-            transform.position += transform.up * (jumpForce * Time.deltaTime);
+            if (distToGround + 0.1 >= transform.position.y)
+            {
+                rb.velocity = new Vector2(0, jumpForce * Time.deltaTime);
+            }
+           
         }
 
         if (FindObjectsOfType<Rock>().Any(x => x.gotPlayerHit))
