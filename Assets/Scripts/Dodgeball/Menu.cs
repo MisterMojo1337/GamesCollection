b4c10d9b6f1e1392e7dcde8e070b1bcedef78544
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-07-20 12:04:00 +0200

    Sprites und Hitboxes for Game

---
+++ Assets/Scripts/Dodgeball/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    
+    public void StartCatchGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartDodgeBall()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
