5ef2fc6761c80498c4d16277caa6013e171f5a31
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-08-14 16:03:01 +0200

    Sachen gemacht

---
+++ Assets/Scripts/MP_Selector/GetControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetControls : MonoBehaviour {
+
+    public Text inpJump;
+    public Text inpLeft;
+    public Text inpRight;
+
+
+    public void Ready()
+    {
+        Debug.Log(inpJump.text + " " + inpLeft.text + " " + inpRight.text);
+    }
+}
