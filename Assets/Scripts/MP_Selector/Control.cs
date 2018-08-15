28f701e0f8637ad7dae04092dccd195ea6fead32
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-08-15 15:26:49 +0200

    JSON Unterstützung eingefügt

---
+++ Assets/Scripts/MP_Selector/Control.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.MP_Selector
+{
+
+    [Serializable]
+    public struct Control
+    {
+            public string Id;
+            public string InputJump;
+            public string InputLeft;
+            public string InputRight;
+    }
+}
--- Assets/Scripts/MP_Selector/GetControls.cs
+++ Assets/Scripts/MP_Selector/GetControls.cs
@@ -2,16 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using Assets.Scripts.MP_Selector;
+using System.Linq;
+using System;
 
 public class GetControls : MonoBehaviour {
 
-    public Text inpJump;
-    public Text inpLeft;
-    public Text inpRight;
+    public InputField inpJump;
+    public InputField inpLeft;
+    public InputField inpRight;
 
+    public GameObject rdyBtn;
+    public GameObject unRdyBtn;
+
+    private string jsonPath;
+    private string jsonString;
+    private int i = 0;
+    private Control[] playerList = new Control[4];
+
+    private void Start()
+    {
+        jsonPath = Application.streamingAssetsPath + "/Controls.json";
+        jsonString = File.ReadAllText(jsonPath);
+        playerList= JsonHelper.FromJson<Control>(jsonString);
+        var objName = gameObject.name;
+
+        foreach (var player in playerList)
+        {
+            if (objName == player.Id)
+            {
+                break;
+            }
+            i += 1;
+        }
+
+        JsonToTextbox(playerList[i]);
+    }
 
     public void Ready()
     {
-        Debug.Log(inpJump.text + " " + inpLeft.text + " " + inpRight.text);
+        rdyBtn.SetActive(false);
+        unRdyBtn.SetActive(true);
+        TextboxToJson();
+    }
+
+    public void NotReady()
+    {
+        rdyBtn.SetActive(true);
+        unRdyBtn.SetActive(false);
+    }
+
+    private void TextboxToJson()
+    {
+        playerList[i].InputJump = inpJump.text;
+        playerList[i].InputLeft = inpLeft.text;
+        playerList[i].InputRight = inpRight.text;
+
+        JsonHelper.ToJson(playerList, true);
+    }
+
+    private void JsonToTextbox(Control player)
+    {
+        inpJump.text = player.InputJump;
+        inpLeft.text = player.InputLeft;
+        inpRight.text = player.InputRight;
     }
 }
