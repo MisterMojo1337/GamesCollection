d3568e722695272b2e66379a35e5f4e02d63f672
Maximilian Schwitalle <mschwitalle@web.de>
2019-06-20 19:09:22 +0200

    ?

---
+++ Assets/Scripts/Dodgeball/Bomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public GameObject particles;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInChildren<CharacterControls>() != null)
+        {
+            collision.GetComponentInChildren<CharacterControls>().healthPoints -= 50;
+        }
+
+        Instantiate(particles, transform.position - new Vector3(0.1f, 0.1f), transform.rotation);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Dodgeball/Spawn.cs
+++ Assets/Scripts/Dodgeball/Spawn.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Linq;
 using UnityEngine;
 
@@ -8,6 +7,7 @@ public class Spawn : MonoBehaviour {
     public Transform spawnPoint;
     public GameObject rock;
     public GameObject healKit;
+    public GameObject bomb;
     public bool spawning = true;
 
     private int playerCount;
@@ -39,10 +39,15 @@ public class Spawn : MonoBehaviour {
                 if (spawnChance > 99.5)
                 {
                     Instantiate(healKit, transform.position, transform.rotation);
-                } else
+                } else if(spawnChance > 98)
+                {
+                    Instantiate(bomb, spawnPoint.position, spawnPoint.rotation);
+                }
+                else
                 {
                     Instantiate(rock, spawnPoint.position, spawnPoint.rotation);
                 }
+
             }            
             yield return new WaitForSeconds(spawnChance/100);
         }        
